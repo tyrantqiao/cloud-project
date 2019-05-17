0e245e01117ba5b8fe907bdc968fb07c42e789ee
tyrantqiao <tyrantqiao@icloud.com>
2019-05-17 13:51:32 +0800

    feat(cloud-config): springcloud-config-server
    本地config server实现了，端口为8888，可以访问到所设定的配置文件之类的，下一步是做集群化

---
+++ cloud-config/src/main/java/com/tyrantqiao/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.tyrantqiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author tyrantqiao
+ * date: 2019/5/17
+ * blog: tyrantqiao.com
+ * contact: tyrantqiao@icloud.com
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
